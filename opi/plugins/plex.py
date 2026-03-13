45884b3bde028de434c60964785f7de81034e9ca
Дмитрий Марков <13werwolf13@mail.ru>
2026-03-13 10:31:58 +0500

    plex repo update

--- opi/plugins/plex.py
+++ opi/plugins/plex.py
@@ -3,7 +3,7 @@ from opi.plugins import BasePlugin
 
 class PlexMediaServer(BasePlugin):
 	main_query = 'plex'
-	description = 'Plex Media Server (See OSS alternative jellyfin)'
+	description = 'Plex Media Server (See OSS alternative jellyfin or emby)'
 	queries = ['plex', 'plexmediaserver']
 
 	@classmethod
@@ -12,10 +12,10 @@ class PlexMediaServer(BasePlugin):
 			return
 
 		opi.add_repo(
-			filename = 'PlexRepo',
-			name = 'PlexRepo',
-			url = 'https://downloads.plex.tv/repo/rpm/$basearch/',
-			gpgkey = 'https://downloads.plex.tv/plex-keys/PlexSign.key'
+			filename = 'plex',
+			name = 'PlexTv',
+			url = 'https://repo.plex.tv/rpm/',
+			gpgkey = 'https://downloads.plex.tv/plex-keys/PlexSign.v2.key'
 		)
 
 		opi.install_packages(['plexmediaserver'])
